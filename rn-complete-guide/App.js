86d5f4c477a3fbf625056f0a995968a04249102c
Arafat Hossain <arafat6462@gmail.com>
2022-11-24 15:33:04 +0600

    Goals

---
+++ rn-complete-guide/App.js
@@ -0,0 +1,81 @@
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList,
+} from "react-native";
+
+// Scrollview render all item at a same time. for short list it good but
+// long list like youtube app never use scrollviw. it will slow the app.
+export default function App() {
+  const [enterGoal, setEnterGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnterGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    // setCourseGoals([...courseGoals, enterGoal]);
+    // courseGoals not garentied to return latels list but below line dose.
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      { key: Math.random().toString(), value: enterGoal },
+    ]);
+    console.log(courseGoals);
+  };
+
+  return (
+    // style takes javascript object (key value) in styel={{}}
+    <View style={styels.screen}>
+      <View style={styels.inputContainer}>
+        <TextInput
+          placeholder="course Goal"
+          style={styels.input}
+          onChangeText={goalInputHandler}
+          value={enterGoal}
+        />
+        {/* Without () function call it will not call in first load */}
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+
+      <FlatList
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <View style={styels.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      ></FlatList>
+    </View>
+  );
+}
+
+const styels = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    width: "80%",
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#bbb",
+    borderColor: "balck",
+    borderWidth: 1,
+  },
+});
---
+++ rn-complete-guide/components/GoalItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+function GoalItem(props) {
+  return (
+    // <View style={styels.listItem}>
+    //   <Text>{}</Text>
+    // </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#bbb",
+    borderColor: "balck",
+    borderWidth: 1,
+  },
+});
+export default GoalItem;
